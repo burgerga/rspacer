c16a00915cea6777fe5f362e9365ad9984d3da7c
Gerhard Burger <burger.ga@gmail.com>
2024-06-05 12:00:54 +0200

    Export api key/url management functions

--- R/api.R
+++ R/api.R
@@ -1,23 +1,77 @@
+#'
+#' Get the RSpace API URL
+#'
+#' Gets the RSpace API URL from the `RSPACE_API_URL` environment variable.
+#' See [set_api_url()] on how to set it.
+#'
+#' @seealso [set_api_url()]
+#' @export
 get_api_url <- function() {
   api_url <- Sys.getenv("RSPACE_API_URL")
   if (identical(api_url, "")) {
     cli::cli_abort(c("Please set env var {.envvar RSPACE_API_URL} to your RSpace API url",
-      "(this is likely the url of your RSpace instance followed by {.code api/v1})."
+      "See {.fun rspacer::set_api_url} for more information."
     ))
   }
 
   api_url
 }
 
+#'
+#' Set the `RSPACE_API_URL` environment variable
+#'
+#' Sets the `RSPACE_API_URL` environment variable to the provided RSpace API URL.
+#' The RSpace API URL is likely the URL of your RSpace instance followed by `api/v1`.
+#'
+#' This will only set the environment variable for the current session,
+#' to set it permanently, add `RSPACE_API_URL=<your_api_url_here>` to your `.Renviron` file,
+#' for example, using [usethis::edit_r_environ()].
+#'
+#' @param rspace_api_url The RSpace API URL
+#'
+#' @examples
+#' \dontrun{
+#' set_api_url("https://leiden.researchspace.com/api/v1")
+#' }
+#' @export
+set_api_url <- function(rspace_api_url) {
+  Sys.setenv(RSPACE_API_URL = rspace_api_url)
+}
+
+#'
+#' Get the RSpace API key
+#'
+#' Gets the RSpace API key from the `RSPACE_API_KEY` environment variable.
+#' See [set_api_key()] on how to set it.
+#'
+#' @seealso [set_api_key()]
+#' @export
 get_api_key <- function() {
   api_key <- Sys.getenv("RSPACE_API_KEY")
   if (identical(api_key, "")) {
     cli::cli_abort(c("Please set env var {.envvar RSPACE_API_KEY} to your RSpace API key",
-      "(see {.strong Authentication with an API key} at {.url https://community.researchspace.com/public/apiDocs})."
+      "See {.fun rspacer::set_api_key} for more information."
     ))
   }
 
   api_key
+}
+
+#'
+#' Set the `RSPACE_API_KEY` environment variable
+#'
+#' Sets the `RSPACE_API_KEY` environment variable to the provided RSpace API key.
+#' To create an API key go to 'Manage API Key' section of your profile page (MyRSpace -> Profile).
+#'
+#' This will only set the environment variable for the current session,
+#' to set it permanently, add `RSPACE_API_KEY=<your_api_key_here>` to your `.Renviron` file,
+#' for example, using [usethis::edit_r_environ()].
+#'
+#' @param rspace_api_key Your RSpace API key
+#'
+#' @export
+set_api_key <- function(rspace_api_key) {
+  Sys.setenv(RSPACE_API_KEY = rspace_api_key)
 }
 
 rspace_error_body <- function(resp) {
