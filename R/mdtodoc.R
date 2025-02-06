ada787447f13e2e7706d13751895fc8381a25e16
Gerhard Burger <burger.ga@gmail.com>
2025-02-06 23:32:01 +0100

    Allow multiple attachments
    * Also simplify documentation
    
    Fixes #21

--- R/mdtodoc.R
+++ R/mdtodoc.R
@@ -70,22 +70,35 @@ excel_to_doc_body <- function(path, document_name = NULL, verbose = T, file_type
   )
 }
 
-attachment_upload <- function(doc_body, attachment, api_key) {
-  # Test if the attachment list has the correct format
-  if (!is.list(attachment)) cli::cli_abort(message = c("x" = "attachment is not provided as a list"))
-  if (!identical(sort(names(attachment)), c("field", "path"))) cli::cli_abort(message = c("x" = "attachment is either missing the field number or the path"))
-  if (as.numeric(attachment$field) > length(doc_body$fields)) cli::cli_abort(message = c("x" = stringr::str_glue("attachment field number is higher than the total number of fields: {length(doc_body$fields)}")))
+attachment_upload <- function(doc_body, attachments, api_key) {
+  # QC
+  if (!is.data.frame(attachments)) cli::cli_abort(message = c("x" = "attachment is not provided as a tibble/data.frame"))
+  if (length(setdiff(c("field", "path"), names(attachments))) > 0) cli::cli_abort(message = c("x" = "attachments df is missing the `field` or `path` column"))
+  if (any(attachments$field < 1)) cli::cli_abort(message = c("x" = stringr::str_glue("attachments field number should be > 0")))
+  if (any(attachments$field > length(doc_body$fields))) cli::cli_abort(message = c("x" = stringr::str_glue("attachments field is higher than the total number of fields ({length(doc_body$fields)})")))
 
-  # Upload the attachment and add its name and path to to doc_body
-  json <- file_upload(attachment$path, api_key)
-  doc_body$fields[[attachment$field]]$content <- glue::glue(
-    doc_body$fields[[attachment$field]]$content,
-    "<p>Inserted <fileId={json$id}></p>"
-  )
+  attachments <- attachments |>
+    dplyr::distinct() |> # filter out duplicates
+    dplyr::rowwise() |>
+    dplyr::mutate(rspace_id = file_upload(.data$path, api_key)$id)
+
+  attachments_res <- attachments |>
+    dplyr::mutate(html = glue::glue("<fileId={.data$rspace_id}>")) |>
+    dplyr::group_by(.data$field) |>
+    dplyr::summarize(html = paste0(.data$html, collapse = "\n"))
+
+  for(i in 1:nrow(attachments_res)) {
+    doc_body$fields[[attachments_res$field[i]]]$content <- glue::glue(
+      doc_body$fields[[attachments_res$field[i]]]$content,
+      "<p><b>Attachments:</b></p>",
+      attachments_res$html[i]
+    )
+  }
+
   return(doc_body)
 }
 
-add_information_to_doc_body <- function(doc_body, template_id = NULL, folder_id = NULL, tags = NULL, attachment = NULL, api_key = get_api_key()) {
+add_information_to_doc_body <- function(doc_body, template_id = NULL, folder_id = NULL, tags = NULL, attachments = NULL, api_key = get_api_key()) {
   if (!is.null(template_id)) {
     form_id <- parse_rspace_id(doc_to_form_id(template_id, verbose = F))
     doc_body$form <- list(id = form_id)
@@ -99,8 +112,8 @@ add_information_to_doc_body <- function(doc_body, template_id = NULL, folder_id 
     doc_body$tags <- paste(tags, collapse = ",")
   }
 
-  if (!is.null(attachment)) {
-    doc_body <- attachment_upload(doc_body, attachment, api_key)
+  if (!is.null(attachments)) {
+    doc_body <- attachment_upload(doc_body, attachments, api_key)
   }
 
   # The API wants a plain array -> remove the names
@@ -117,11 +130,11 @@ add_information_to_doc_body <- function(doc_body, template_id = NULL, folder_id 
 #' @param template_id document id of the RSpace template used. Overwritten by the template of existing_document_id if that one is specified.
 #' @param folder_id folder_id in which the document will be created (can be a notebook)
 #' @param tags vector of tags to apply to the document
-#' @param attachment attachment to attach to one of the fields, e.g., `list(field = 7, path = "file.txt")`
+#' @param attachments attachments to attach to the fields in tibble/data.frame form (one attachment per row), e.g., `tibble(field = 7, path = "file.txt")`
 #' @param existing_document_id if you want to replace a document by a new one, specify the current identifier here.
 #' @inheritParams api_status
 #' @export
-document_create_from_html <- function(path, template_id = NULL, folder_id = NULL, tags = NULL, attachment = NULL,
+document_create_from_html <- function(path, template_id = NULL, folder_id = NULL, tags = NULL, attachments = NULL,
                                       existing_document_id = NULL, api_key = get_api_key()) {
   doc_body <- html_to_doc_body(path, verbose = F)
 
@@ -148,7 +161,7 @@ document_create_from_html <- function(path, template_id = NULL, folder_id = NULL
     doc_body$fields <- put_all_fields_in_one_field(doc_body$fields)
   }
   # Add tags, form ID and attachments to doc_body
-  doc_body <- add_information_to_doc_body(doc_body, template_id, folder_id, tags, attachment, api_key)
+  doc_body <- add_information_to_doc_body(doc_body, template_id, folder_id, tags, attachments, api_key)
 
   # Create or replace the document
   if (is.null(existing_document_id)) {
@@ -166,16 +179,14 @@ document_create_from_html <- function(path, template_id = NULL, folder_id = NULL
 #' This function retrieves the current document, and adds text to fields specified by
 #' h2 html headers.
 #'
-#' @param path html document to be uploaded
 #' @param existing_document_id document identifier of the current RSpace document.
-#' @param tags vector of tags to apply to the document
-#' @param attachment attachment to attach to one of the fields, e.g., `list(field = 7, path = "file.txt")`
 #' @param allow_missing_fields Specify if a mismatch in fields is allowed.
 #' If this is `FALSE`, the html fields cannot be appended to the RSpace document when fields are missing.
 #' If it is `TRUE`, only fields with the same name as in the template will be appended.
 #' @inheritParams api_status
+#' @inheritParams document_create_from_html
 #' @export
-document_append_from_html <- function(path, existing_document_id, tags = NULL, attachment = NULL,
+document_append_from_html <- function(path, existing_document_id, tags = NULL, attachments = NULL,
                                       allow_missing_fields = FALSE, api_key = get_api_key()) {
   # Get the current fields
   current_fields <- doc_get_fields(existing_document_id)
@@ -244,9 +255,9 @@ document_append_from_html <- function(path, existing_document_id, tags = NULL, a
     doc_body$tags <- paste(tags, collapse = ",")
   }
 
-  if (!is.null(attachment)) {
+  if (!is.null(attachments)) {
     # TODO maybe use the columnIndex because I already removed all non-altered field numbers?
-    doc_body <- attachment_upload(doc_body, attachment, api_key)
+    doc_body <- attachment_upload(doc_body, attachments, api_key)
   }
 
   # Replace old fields with new fields
@@ -264,15 +275,12 @@ document_append_from_html <- function(path, existing_document_id, tags = NULL, a
 #' @param document_name specify the name of the RSpace entry. If not specified,
 #' it will be the value in Title, Name, title, or name if that is one of the fields in the Excel document.
 #' If that does not exist, it will be the file name.
-#' @param template_id document id of the RSpace template used
-#' @param folder_id folder_id in which the document will be created (can be a notebook)
-#' @param tags vector of tags to apply to the document
-#' @param attachment attachment to attach to one of the fields, e.g., `list(field = 7, path = "file.txt")`
 #' @param existing_document_id if you want to replace a document by a new one, specify the current identifier here.
 
 #' @inheritParams api_status
+#' @inheritParams document_create_from_html
 #' @export
-document_create_from_excel <- function(path, file_type = NULL, document_name = NULL, template_id = NULL, folder_id = NULL, tags = NULL, attachment = NULL, api_key = get_api_key(), existing_document_id = NULL) {
+document_create_from_excel <- function(path, file_type = NULL, document_name = NULL, template_id = NULL, folder_id = NULL, tags = NULL, attachments = NULL, api_key = get_api_key(), existing_document_id = NULL) {
   doc_body <- excel_to_doc_body(path, document_name = document_name, verbose = F, file_type = file_type)
 
   if (!is.null(existing_document_id)) {
@@ -289,7 +297,7 @@ document_create_from_excel <- function(path, file_type = NULL, document_name = N
     doc_body$fields <- put_all_fields_in_one_field(doc_body$fields)
   }
   # Add tags, form ID and attachments to doc_body
-  doc_body <- add_information_to_doc_body(doc_body, template_id = template_id, folder_id = folder_id, tags = tags, attachment = attachment)
+  doc_body <- add_information_to_doc_body(doc_body, template_id = template_id, folder_id = folder_id, tags = tags, attachments = attachments)
 
   # Create or replace the document
   if (is.null(existing_document_id)) {
